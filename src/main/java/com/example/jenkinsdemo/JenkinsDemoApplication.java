71c0e3bc98addae0f2488f56950ff446655b4d8b
camus <free7hem@gmail.com>
2022-05-17 04:22:08 +0100

    first commit

---
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+	@GetMapping(value="/")
+	public String hi() {
+		return "hi";
+	}
+	
+}
